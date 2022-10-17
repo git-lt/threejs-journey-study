9f94a2fb85bdad3860c37e104df72ff6d61dbb27
liutao <liutaotao@mytijian.com>
2022-10-17 14:38:04 +0800

    feat:18-galaxy-generator

--- 18-galaxy-generator/index.ts
+++ 18-galaxy-generator/index.ts
@@ -1,7 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper'
 import stats from '../common/stats'
 import * as dat from 'lil-gui'
 import { listenResize } from '../common/utils'
@@ -12,41 +11,108 @@ const sizes ={
 }
 const aspect = sizes.width/sizes.height;
 
+// Canvas
 const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement;
-
-const textureLoader = new THREE.TextureLoader();
-const particleTexture = textureLoader.load('https://gw.alicdn.com/imgextra/i3/O1CN01DO6Ed61QtcMKsVnK2_!!6000000002034-2-tps-56-56.png')
 
 // Scene
 const scene = new THREE.Scene();
 
-const pointMaterial = new THREE.PointsMaterial({
-  size: .1,
-  sizeAttenuation: true
-})
-
-// pointMaterial.color = new THREE.Color('#f8c')
-pointMaterial.alphaMap = particleTexture;
-pointMaterial.transparent = true;
-pointMaterial.depthWrite = false;
-// 混合模式
-pointMaterial.blending = THREE.AdditiveBlending;
-// 是否使用顶点着色
-pointMaterial.vertexColors = true;
-
-const particlesGeometry = new THREE.BufferGeometry();
-const count = 500;
-const positions = new Float32Array(count * 3)
-const colors = new Float32Array(count * 3)
-for(let i = 0;i < count * 3; i++){
-  positions[i] = (Math.random() - .5) * 5;
-  colors[i] = Math.random();
+/**
+ * Galaxy
+ */
+const parameters = {
+  count: 10000,
+  size: .02,
+  radius: 5,
+  branches: 3,
+  spin: 1,
+  randomness: .2,
+  randomnessPower: 3,
+  insideColor: '#ff6030',
+  outsideColor: '#1b3984',
 }
-particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
 
-const particles = new THREE.Points(particlesGeometry, pointMaterial)
-scene.add(particles)
+let geometry: THREE.BufferGeometry;
+let material: THREE.PointsMaterial;
+let points: THREE.Points<THREE.BufferGeometry, THREE.PointsMaterial>;
+
+const generatorGalaxy = () => {
+  if(points){
+    geometry.dispose();
+    material.dispose();
+    scene.remove(points)
+  }
+
+  const colorInside = new THREE.Color(parameters.insideColor)
+  const colorOutside = new THREE.Color(parameters.outsideColor)
+
+  // Geometry
+  geometry = new THREE.BufferGeometry();
+
+  const position = new Float32Array(parameters.count * 3)
+  const colors = new Float32Array(parameters.count * 3);
+  
+  for(let i = 0; i< parameters.count * 3; i++){
+    const i3 = i * 3;
+    // position[i3] = (Math.random() - .5) * 3
+    // position[i3 + 1] = (Math.random() - .5) * 3
+    // position[i3 + 2] = (Math.random() - .5) * 3
+
+    // 生成在x轴上
+    // const radius = Math.random() * parameters.radius;
+    // position[i3] = radius
+    // position[i3 + 1] = 0
+    // position[i3 + 2] = 0
+
+    // 生成在多个分支上
+    const radius = Math.random() * parameters.radius;
+    const branchesAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+    // 偏转角度
+    const spinAngle = radius * parameters.spin;
+    // 随机扩散
+    
+    const randomX =Math.random() ** parameters.randomnessPower
+      * (Math.random() < 0.5 ? 1 : -1)
+      * parameters.randomness
+      * radius
+    const randomY =Math.random() ** parameters.randomnessPower
+      * (Math.random() < 0.5 ? 1 : -1)
+      * parameters.randomness
+      * radius
+    const randomZ =Math.random() ** parameters.randomnessPower
+      * (Math.random() < 0.5 ? 1 : -1)
+      * parameters.randomness
+      * radius
+
+    position[i3] = Math.cos(branchesAngle + spinAngle) * radius + randomX;
+    position[i3 + 1] = 0 + randomY
+    position[i3 + 2] = Math.sin(branchesAngle + spinAngle) * radius + randomZ
+
+    // 设置每个顶点的颜色
+    const mixedColor = colorInside.clone();
+    mixedColor.lerp(colorOutside, radius / parameters.radius);
+    colors[i3] = mixedColor.r;
+    colors[i3+1] = mixedColor.g;
+    colors[i3+2] = mixedColor.b;
+  }
+
+  geometry.setAttribute('position', new THREE.BufferAttribute(position, 3))
+  geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+  // Material
+  material = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true,
+  })
+
+  points = new THREE.Points(geometry, material);
+  scene.add(points);
+}
+
+generatorGalaxy();
 
 
 // Lights
@@ -86,7 +152,6 @@ const tick = () => {
   stats.begin();
   const elapsedTime = clock.getElapsedTime();
 
-  pointMaterial.needsUpdate = true;
   controls.update();
   renderer.render(scene, camera)
 
@@ -100,5 +165,3 @@ const gui = new dat.GUI();
 
 gui.add(controls, 'autoRotate')
 gui.add(controls, 'autoRotateSpeed', .1, 10, .01)
-gui.add(pointMaterial, 'size', .01, .1, .001)
-gui.add(pointMaterial, 'sizeAttenuation')
